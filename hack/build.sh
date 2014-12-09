0b9fc89e60483d0adaf21525250726a5ee006d7e
Michal Fojtik <mfojtik@redhat.com>
2014-12-09 17:19:06 +0100

    typo

--- hack/build.sh
+++ hack/build.sh
@@ -12,11 +12,11 @@ TARGET_IMAGE="172.121.17.3:5001/${IMAGE_NAME}:${IMAGE_TAG}"
 pushd /build >/dev/null
 # Build a static Go binary and create minimal Docker image from it
 CGO_ENABLED=0 go get -a -ldflags '-s' ${GO_SOURCE}
-(cd /gopath/bin && tar cv ${GO_BINARY}) | docker import - ${IMAGE_INAME}-scratch
+(cd /gopath/bin && tar cv ${GO_BINARY}) | docker import - ${IMAGE_NAME}-scratch
 
 # The final image Dockerfile
 cat > Dockerfile <<- EOF
-FROM ${IMAGE_INAME}-scratch
+FROM ${IMAGE_NAME}-scratch
 EXPOSE 8080
 CMD ["${GO_BINARY}"]
 EOF
